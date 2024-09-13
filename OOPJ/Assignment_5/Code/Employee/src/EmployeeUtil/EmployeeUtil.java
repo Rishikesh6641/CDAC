ee0c56a88741c2358042a4c60ddd66d422323131
Rishikesh Jawale <94427560+Rishikesh6641@users.noreply.github.com>
2024-09-13 15:24:06 +0530

    Update EmployeeUtil.java

--- OOPJ/Assignment_5/Code/Employee/src/EmployeeUtil/EmployeeUtil.java
+++ OOPJ/Assignment_5/Code/Employee/src/EmployeeUtil/EmployeeUtil.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EmployeeUtil {
 	
 	private Employee emp;
-	Scanner sc=new Scanner(System.in);
+	static Scanner sc=new Scanner(System.in);
 	
 	public EmployeeUtil()
 	{
@@ -20,10 +20,13 @@ public class EmployeeUtil {
 
 		System.out.println("Name:");
 		emp.setName(sc.nextLine());
-		sc.nextLine();
+		
 		
 		System.out.println("Enter Salary:");
 		emp.setIn_salary(sc.nextDouble());
+		
+		System.out.println("Enter What per You want to raise the Salary:");
+		Employee.setPer(sc.nextFloat());
 		
 	}
 	
@@ -33,15 +36,31 @@ public class EmployeeUtil {
 		System.out.println("Name: "+emp.getName());
 		System.out.println("Salary: "+emp.getIn_salary());
 	}
-	/*public void CompanyRecord()
+	public void CompanyRecord()
 	{
 		System.out.println("Total Employee: "+Employee.getTotal_employee());
 		System.out.println("Total Expense: "+Employee.getTotal_salary());
 		System.out.println("Raise Per: "+Employee.getPer());
-	}*/	
+	}
+	public static int menulist()
+	{
+		System.out.println("0.Exit");
+		System.out.println("1.Accept Record of Employee");
+		System.out.println("2.Raise Salary");
+		System.out.println("3.Print Recors Of Employee");
+		System.out.println("4.Print Record of Company");
+
+		System.out.println("Enter Choice:");
+		
+		return sc.nextInt();
+	}
 	public Employee getemp()
 	{
 		return emp;
+	}
+	public void closeResource()
+	{
+		sc.close();
 	}
 
 }
